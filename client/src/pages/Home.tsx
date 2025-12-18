2d2c86887c6b7e83b79ce1b8dcb4679ecf66beff
Usama Hassan <ahmarkhan1233456@gmail.com>
2025-12-18 18:03:46 +0500

    feat: base setup

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+  return (
+    <div>
+      <Button variant="secondary">ShadCN Button</Button>
+    </div>
+  )
+}
+
+export default Home
